4536c1991d1b91f53293f0da0a500a390213871e
mohamed mansour <137690348+Mansour3300@users.noreply.github.com>
2023-08-20 02:27:31 +0300

    push last update

--- app/Http/Controllers/Provider/ServiceController.php
+++ app/Http/Controllers/Provider/ServiceController.php
@@ -16,7 +16,7 @@ class ServiceController extends Controller
     {
         $service = Service::all();
         $resource = ServiceResource::collection($service);
-        return response()->json(['success'=>'true','data'=>$resource]);
+        return response()->json(['status'=>'success','data'=>$resource]);
     }
 
 
@@ -24,7 +24,7 @@ class ServiceController extends Controller
     {
         $service = Service::where('provider_id',auth()->guard('developer')->id())->get();
         $resource = ServiceResource::collection($service);
-        return response()->json(['success'=>'true','data'=>$resource]);
+        return response()->json(['status'=>'success','data'=>$resource]);
     }
 
     /**
@@ -34,7 +34,7 @@ class ServiceController extends Controller
     {
         $service = Service::where('provider_id',auth()->guard('developer')->id())->findorfail($id)->get();
         $resource = ServiceResource::make($service);
-        return response()->json(['success'=>'true','data'=>$resource]);
+        return response()->json(['status'=>'success','data'=>$resource]);
     }
 
     public function acceptService($id){
